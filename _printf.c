16f51e4e7ea32c608394d164526f040f5eec1c33
mahmoud94ha <91053929+mahmoud94ha@users.noreply.github.com>
2021-11-09 18:10:36 +0100

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 /**
  * _printf - print all (printf reecreation)
  * @format: format specifier
@@ -34,26 +34,7 @@ else if (format[i + 1] == 's')
 len = len + print_string(va_arg(arg, char *));
 i = i + 2;
 }
-/*else if (format[i + 1] == 'b')
-{
-len = len + print_int(tobin(va_arg(arg, unsigned int)));
-i = i + 2;
-}
-else if (format[i + 1] == 'o')
-{
-len = len + print_int(tooct(va_arg(arg, int)));
-i = i + 2;
-}
-else if (format[i + 1] == 'x')
-{
-len = len + print_string(tohex(va_arg(arg, int)));
-i = i + 2;
-}
-else if (format[i + 1] == 'X')
-{
-len = len + print_string(toHEX(va_arg(arg, int)));
-i = i + 2;
-}*/
+
 else if (format[i + 1] == '%')
 {
 len = len + _putchar('%');
