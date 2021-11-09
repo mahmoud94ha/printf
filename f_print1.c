483768089106884fd121a6005d72bde76f514cb0
mahmoud94ha <91053929+mahmoud94ha@users.noreply.github.com>
2021-11-09 17:49:55 +0100

    function

---
+++ f_print1.c
@@ -0,0 +1,88 @@
+#include "main.h"
+/**
+ * print_char - print a character
+ * @arg: input
+ * Return: always 1
+ *
+ */
+int print_char(char arg)
+{
+_putchar(arg);
+return (1);
+}
+/**
+ *print_string - print a string
+ *@arg: input
+ * Return: string length
+ */
+int print_string(char *arg)
+{
+int len = 0;
+if (arg == NULL)
+arg = "(null)";
+while (arg[len] != '\0')
+{
+_putchar(arg[len]);
+len++;
+}
+return (len);
+}
+/**
+ *print_int - print a number
+ *@arg: input
+ * Return: int length.
+ */
+int print_int(int n)
+{
+int count = 0, rest = 1;
+int x;
+x = n;
+if (n < 0)
+{
+_putchar('-');
+x = -x;
+count++;
+}
+while ((x / rest) > 9)
+{
+rest = rest * 10;
+}
+while (rest > 0)
+{
+_putchar(x / rest + '0');
+x = (x % rest);
+rest = rest / 10;
+count++;
+}
+return (count);
+}
+/**
+ *print_unsigned_int - print a number
+ *@arg: input
+ * Return: int length.
+ */
+unsigned int print_unsigned_int(unsigned int n)
+{
+unsigned int count = 0, rest = 1;
+unsigned int x;
+unsigned int zero=0;
+x = n;
+if (n < zero)
+{
+_putchar('-');
+x = -x;
+count++;
+}
+while ((x / rest) > 9)
+{
+rest = rest * 10;
+}
+while (rest > 0)
+{
+_putchar(x / rest + '0');
+x = (x % rest);
+rest = rest / 10;
+count++;
+}
+return (count);
+}
